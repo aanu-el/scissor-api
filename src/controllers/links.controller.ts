a7be596642fbca8693ae5ba679b7def40f7f85c7
Peace Aanu <peaceaanu247@gmail.com>
2023-07-17 12:50:02 +0100

    API routes completed

--- src/controllers/links.controller.ts
+++ src/controllers/links.controller.ts
@@ -5,7 +5,10 @@ import { AuthService } from "../services/auth.service";
 require('dotenv').config();
 
 export const getAllLinks: RequestHandler = async (req, res, next) => {
-    const allLinks = await LinkModel.findAll();
+    const user = new AuthService().getUserFromToken(req.headers.authorization);
+    const userUuid = user.userUuid;
+
+    const allLinks = await LinkModel.findAll({ where: { userUuid: userUuid } });
     return res.status(200).json({
         message: "Success",
         data: allLinks
@@ -13,8 +16,11 @@ export const getAllLinks: RequestHandler = async (req, res, next) => {
 }
 
 export const getLinkById: RequestHandler = async (req, res, next) => {
+    const user = new AuthService().getUserFromToken(req.headers.authorization);
+    const userUuid = user.userUuid;
     const id = req.params.id;
-    await LinkModel.findOne({ where: { id: id } })
+
+    await LinkModel.findOne({ where: { id: id, userUuid: userUuid } })
         .then((link) => {
             return res.status(200).json({
                 message: "Success",
@@ -70,7 +76,10 @@ export const createLink: RequestHandler = async (req, res, next) => {
     } else {
         backHalf = new LinkService().randomstring();
     }
-    
+
+    const finalUrl = `${base_domain}/${backHalf}`
+    const qrCode = await new LinkService().generateQR(finalUrl);
+
     // save to db
     await LinkModel.create(
         {
@@ -78,7 +87,8 @@ export const createLink: RequestHandler = async (req, res, next) => {
             url: url,
             backHalf: backHalf,
             customDomain: base_domain,
-            finalUrl: `${base_domain}/${backHalf}`
+            finalUrl: `${base_domain}/${backHalf}`,
+            qrCode: qrCode
         })
         .then((link) => {
             return res.status(201).json({
@@ -92,12 +102,61 @@ export const createLink: RequestHandler = async (req, res, next) => {
 }
 
 export const updateLink: RequestHandler = async (req, res, next) => {
-    const link = req.body;
+    const user = new AuthService().getUserFromToken(req.headers.authorization);
+    const userUuid = user.userUuid;
+    const base_domain = process.env.BASE_DOMAIN;
 
+    const id = req.params.id;
+    const backHalf = req.body.backHalf
+
+    try {
+        let link = await LinkModel.findOne({ where: { id: id, userUuid: userUuid } })
+
+        if (!link) {
+            return res.status(404).json({ status: false, message: "link not found" })
+        }
+
+        const updatedUrl = `${base_domain}/${backHalf}`
+
+        link.set({
+            backHalf: backHalf,
+            finalUrl: updatedUrl,
+            qrCode: await new LinkService().generateQR(updatedUrl)
+        })
+
+        await link.save();
+
+        return res.status(200).json({ status: true, data: link })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: 'An error occurred'
+        })
+    }
 }
 
 export const deleteLink: RequestHandler = async (req, res, next) => {
-    const link = req.body;
+    const id = req.params.id;
+    const user = new AuthService().getUserFromToken(req.headers.authorization);
+    const userUuid = user.userUuid;
 
+    try {
+        let link = await LinkModel.findOne({ where: { id: id, userUuid: userUuid } })
+
+        if (!link) {
+            return res.status(404).json({ status: false, message: "link not found" })
+        }
+
+        await link.destroy()
+        return res.status(200).json({ status: true, message: "Link deleted", data: link })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: 'An error occurred'
+        })
+    }
 }
 
