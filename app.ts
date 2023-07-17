54813326ad47a1195812d472b760f2c2f37a905b
Peace Aanu <peaceaanu247@gmail.com>
2023-07-17 15:47:44 +0100

    use helmet

--- app.ts
+++ app.ts
@@ -1,27 +1,32 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import helmet from "helmet";
+require('dotenv').config();
+
+/* Import Modules  */
 import connection from './src/config/db.config';
 import LinksRouter from './src/routes/links.route';
 import Auth from './src/routes/auth.route';
 import UrlRedirectRouter from './src/routes/index.route';
-require('dotenv').config();
 
-const app: express.Application = express();
 const PORT = process.env.PORT;
+const app: express.Application = express();
+
+app.use(helmet()); //=> Use Helmet!
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.use('/api/v1/auth', Auth); //==> Authentication
-app.use('/api/v1', LinksRouter);
-app.use('', UrlRedirectRouter);
+app.use('/api/v1/auth', Auth); //==> Authentication Router
+app.use('/api/v1', LinksRouter); //==> Links Router
+app.use('', UrlRedirectRouter); //=> Default Url Redirect Router
 
 
 app.get('/api/v1', (req: Request, res: Response) => res.json({
     'status': 'success',
     'message': 'Welcome to Scissor'
-}))
+})); //=> Home Route
 
 
 
